7ca216c02d5971a4562f78781e3e9263187c3974
Raiki8191 <yoshiraiki@gmail.com>
2025-01-29 08:47:37 +0000

    適切なコミットメッセージ

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
 
+
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
 });
